68f953bb8e18945db9ecb3fc5e64a367bd19312d
DonaldB89 <84141300+DonaldB89@users.noreply.github.com>
2022-05-27 10:50:05 +0100

    Create results_page.py

---
+++ results_page.py
@@ -0,0 +1,22 @@
+import streamlit as st#
+import pickle
+import numpy as np
+import pandas as pd
+from PIL import Image
+from st_aggrid import AgGrid, GridOptionsBuilder
+
+df = pd.read_csv("submission.csv")
+df = pd.read_csv('submission.csv')
+df = df.loc[df["Category"] !='N']
+df = df.drop(labels=["clean_text", "predict_prob","Unnamed:"], axis=1)
+df = df.rename(columns={'Text': 'Tweets', 'Category': 'Important?'})
+
+st.title("Twitter Tracker")
+
+st.write("""### The latest trending news sources""")
+
+st.table(df)
+
+#image
+image = Image.open('8MS logo2.jpg')
+st.image(image,width=300)

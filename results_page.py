4a76f77ae897646502905579c861c52a55bd1d05
Donald Barclay <84141300+DonaldB89@users.noreply.github.com>
2022-06-01 11:23:41 +0100

    Update results_page.py

--- results_page.py
+++ results_page.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+df = pd.read_csv("https://raw.githubusercontent.com/DonaldB89/Twitter-Tracker/main/submission.csv")
+
 st.title("Twitter Tracker")
 
 st.write("""### The latest trending news sources""")

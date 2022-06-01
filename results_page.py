325ec1ab758fcb7b7848e0e41c90e1387b67b8d3
Donald Barclay <84141300+DonaldB89@users.noreply.github.com>
2022-06-01 11:13:29 +0100

    Create results_page.py

---
+++ results_page.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+df = pd.read_csv("submission.csv")
+df = pd.read_csv('submission.csv')
+df = df.loc[df["Category"] !='N']
+df = df.drop(labels=["clean_text", "predict_prob"], axis=1)
+df = df.rename(columns={'Text': 'Tweets', 'Category': 'Important?'})
+
+st.title("Twitter Tracker")
+
+st.write("""### The latest trending news sources""")
+
+st.table(df)
+
+#image
+image = Image.open('8MS logo2.jpg')
+st.image(image,width=300)
